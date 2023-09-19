17ea70dc379d8bba1bce5794dccfc1ed8c99492a
gedeastu <gede.astu@gmail.com>
2023-09-19 21:03:20 +0700

    up

--- Objects&Array.js
+++ Objects&Array.js
@@ -8,8 +8,10 @@ let Char = {
     countLevel: function () { return true }
 }
 console.log(Char.countLevel)
+console.log(Char["skill"])
+console.log(Char)
 
-// Basic Object with new Object :
+// Basic Object with new Object() :
 let char =  new Object({
     name: "Kont",
     gender: "Female",
@@ -18,3 +20,42 @@ let char =  new Object({
     countLevel: function () { return true }
 })
 console.log(char["name"])
+console.log(char)
+
+// Props Object
+Char["skill"] = () => {
+    for (let n = 0; n < 10; n++){
+        console.log(n)
+    }
+}
+
+// Arrays
+//Basic Array General :
+let array = ['Jok','Mok','Lok','Pok'];
+
+// Basic Array with new Array() :
+let array2 = new Array('Jok','Mok','Lok','Pok')
+
+
+// Mixing Session
+let object1 = {id: 1, name:'juke',age:23}
+let object2 = {id: 2, name:'luke',age:33}
+let object3 = {id: 3, name:'Pole',age:24}
+
+let arrayTakeObject = [object1,object2,object3]
+console.log(arrayTakeObject)
+
+console.log('the first Object: ')
+console.log(arrayTakeObject[0])
+
+console.log('the second Object: ')
+console.log(arrayTakeObject[1])
+
+console.log('the third Object: ')
+console.log(arrayTakeObject[2])
+
+// adding object with .push()
+const object4 = {id:1, name:'cole',age:43}
+arrayTakeObject.push(object4)
+
+
